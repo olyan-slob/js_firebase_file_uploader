980e9d45b99a251c867770aa275ec54cac54cc37
olya <olgaslobodchuk@gmail.com>
2021-04-30 16:02:23 +0300

    add firebase upload

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-import { upload } from "./upload";
+import firebase from "firebase/app";
+import "firebase/storage";
+import { upload } from "./upload.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkXXjPeSAPTsBs23659x1_7yq3b_4fQso",
+  authDomain: "upload-files-180ca.firebaseapp.com",
+  projectId: "upload-files-180ca",
+  storageBucket: "upload-files-180ca.appspot.com",
+  messagingSenderId: "20703941589",
+  appId: "1:20703941589:web:db460482ff0fdadc80635c",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
 
 upload("#file", {
   multi: true,
   accept: [".png", ".jpg", ".jpeg", ".gif"],
+  onUpload(files, blocks) {
+    files.forEach((file, index) => {
+      const ref = storage.ref(`images/${file.name}`);
+      const task = ref.put(file);
+
+      task.on(
+        "state_changed",
+        (snapshot) => {
+          const percentage =
+            ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(
+              0
+            ) + "%";
+          const block = blocks[index].querySelector(".preview-info-progress");
+          block.textContent = percentage;
+          block.style.width = percentage;
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          task.snapshot.ref.getDownloadURL().then((url) => {
+            console.log("Download URL", url);
+          });
+        }
+      );
+    });
+  },
 });
--- upload.js
+++ upload.js
@@ -7,16 +7,30 @@ function bytesToSize(bytes) {
   return Math.round(bytes / Math.pow(1024, i)) + " " + sizes[i];
 }
 
+const element = (tag, classes = [], content) => {
+  const node = document.createElement(tag);
+
+  if (classes.length) {
+    node.classList.add(...classes);
+  }
+
+  if (content) {
+    node.textContent = content;
+  }
+
+  return node;
+};
+
+function noop() {}
+
 export function upload(selector, options = {}) {
   let files = [];
+  const onUpload = options.onUpload ?? noop;
   const input = document.querySelector(selector);
-  const preview = document.createElement("div");
-
-  preview.classList.add("preview");
-
-  const open = document.createElement("button");
-  open.classList.add("btn");
-  open.textContent = "Open";
+  const preview = element("div", ["preview"]);
+  const open = element("button", ["btn"], "Открыть");
+  const upload = element("button", ["btn", "primary"], "Загрузить");
+  upload.style.display = "none";
 
   if (options.multi) {
     input.setAttribute("multiple", true);
@@ -27,6 +41,7 @@ export function upload(selector, options = {}) {
   }
 
   input.insertAdjacentElement("afterend", preview);
+  input.insertAdjacentElement("afterend", upload);
   input.insertAdjacentElement("afterend", open);
 
   const triggerInput = () => input.click();
@@ -37,8 +52,9 @@ export function upload(selector, options = {}) {
     }
 
     files = Array.from(event.target.files);
-
     preview.innerHTML = "";
+    upload.style.display = "inline";
+
     files.forEach((file) => {
       if (!file.type.match("image")) {
         return;
@@ -48,19 +64,18 @@ export function upload(selector, options = {}) {
 
       reader.onload = (ev) => {
         const src = ev.target.result;
-
         preview.insertAdjacentHTML(
           "afterbegin",
           `
-            <div class="preview-image">
-              <div class="preview-remove" data-name="${file.name}">&times;</div>
-              <img src="${src}" alt="${file.name}" />
-              <div class="preview-info">
-                <span>${file.name}</span>
-                ${bytesToSize(file.size)}
-              </div>
+          <div class="preview-image">
+            <div class="preview-remove" data-name="${file.name}">&times;</div>
+            <img src="${src}" alt="${file.name}" />
+            <div class="preview-info">
+              <span>${file.name}</span>
+              ${bytesToSize(file.size)}
             </div>
-          `
+          </div>
+        `
         );
       };
 
@@ -74,17 +89,34 @@ export function upload(selector, options = {}) {
     }
 
     const { name } = event.target.dataset;
-    files = files.filter((file) => file.name == !name);
+    files = files.filter((file) => file.name !== name);
+
+    if (!files.length) {
+      upload.style.display = "none";
+    }
 
     const block = preview
-      .querySelector(`[data-name="${name}]`)
-      .closest("preview-image");
+      .querySelector(`[data-name="${name}"]`)
+      .closest(".preview-image");
 
     block.classList.add("removing");
-    block.remove();
+    setTimeout(() => block.remove(), 300);
+  };
+
+  const clearPreview = (el) => {
+    el.style.bottom = "4px";
+    el.innerHTML = '<div class="preview-info-progress"></div>';
+  };
+
+  const uploadHandler = () => {
+    preview.querySelectorAll(".preview-remove").forEach((e) => e.remove());
+    const previewInfo = preview.querySelectorAll(".preview-info");
+    previewInfo.forEach(clearPreview);
+    onUpload(files, previewInfo);
   };
 
   open.addEventListener("click", triggerInput);
   input.addEventListener("change", changeHandler);
   preview.addEventListener("click", removeHandler);
+  upload.addEventListener("click", uploadHandler);
 }
